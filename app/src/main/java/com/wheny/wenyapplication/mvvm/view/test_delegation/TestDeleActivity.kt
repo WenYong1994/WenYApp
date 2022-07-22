776668cf9f74e3c3cb9c7cdee9a84068ed7b0914
wenyongwen <wenyongwen@tencent.com>
2022-07-22 14:30:39 +0800

    编写demo

--- app/src/main/java/com/wheny/wenyapplication/mvvm/view/test_delegation/TestDeleActivity.kt
+++ app/src/main/java/com/wheny/wenyapplication/mvvm/view/test_delegation/TestDeleActivity.kt
@@ -3,20 +3,29 @@ package com.wheny.wenyapplication.mvvm.view.test_delegation
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.PixelCopy
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
 import com.chenenyu.router.annotation.InjectParam
 import com.chenenyu.router.annotation.Route
 import com.wheny.wenyapplication.R
-import kotlinx.android.synthetic.main.activity_test_dele.*
 import org.libpag.PAGView
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 import kotlin.reflect.KProperty
+
 
 @Route("TestDeleActivity", "TestDeleActivity2")
 class TestDeleActivity : AppCompatActivity() {
@@ -33,33 +42,51 @@ class TestDeleActivity : AppCompatActivity() {
         WebView(this)
     }
 
+    val webViewPaint by lazy {
+        Paint()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_dele)
         findViewById<ViewGroup>(R.id.web_group).addView(webView)
-        webView.setLayerType(View.LAYER_TYPE_SOFTWARE,null)
-        val group = findViewById<View>(R.id.web_group);
-//        webView.webChromeClient = WebChromeClient()
+        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, webViewPaint)
+//        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, webViewPaint)
+        val startTime = System.currentTimeMillis()
+        webView.loadUrl("https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage/_sample_.a_simple_example.html")
+        webView.webViewClient = object : WebViewClient(){
+            override fun onPageFinished(view: WebView?, url: String?) {
+                webView.setLayerType(View.LAYER_TYPE_HARDWARE,webViewPaint)
+                super.onPageFinished(view, url)
+                val time = System.currentTimeMillis() - startTime
+                Log.e("onPageFinished","times:$time")
+                Toast.makeText(this@TestDeleActivity,"sfafafafa",Toast.LENGTH_SHORT).show()
+            }
+        }
         webView.settings.javaScriptEnabled = true
+        WebView.setWebContentsDebuggingEnabled(true)
         val iv = findViewById<ImageView>(R.id.img)
         val pagView = findViewById<PAGView>(R.id.pag_view)
-        pagView.path ="assets://wish_list_guide_profile.pag"
+        pagView.path = "assets://wish_list_guide_profile.pag"
         pagView.play()
         pagView.setRepeatCount(1)
         findViewById<View>(R.id.btn1).setOnClickListener {
             webView.loadUrl("https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage/_sample_.a_simple_example.html")
         }
         findViewById<View>(R.id.btn).setOnClickListener {
-            val rootView = findViewById<NestedScrollView>(R.id.scroll_view).getChildAt(0)
-            val drawingCache = Bitmap.createBitmap(webView.width,window.decorView.height,Bitmap.Config.ARGB_8888)
+//            val rootView = findViewById<NestedScrollView>(R.id.scroll_view).getChildAt(0)
+            val rootView = window.decorView
+            val drawingCache =
+                Bitmap.createBitmap(webView.width, window.decorView.height, Bitmap.Config.ARGB_8888)
             val canvas = Canvas(drawingCache)
             rootView.draw(canvas)
-            val mergeBitmap = Bitmap.createBitmap(rootView.width,rootView.height,Bitmap.Config.ARGB_8888)
+
+            val mergeBitmap =
+                Bitmap.createBitmap(rootView.width, rootView.height, Bitmap.Config.ARGB_8888)
             val mergeCanvas = Canvas(mergeBitmap)
             val paint = Paint()
-            mergeCanvas.drawBitmap(drawingCache,0f,0f,paint)
-            val pagBitMap = pagView.getBitmap(pagView.width,pagView.height)
+            mergeCanvas.drawBitmap(drawingCache, 0f, 0f, paint)
+            val pagBitMap = pagView.getBitmap(pagView.width, pagView.height)
 
             val pagLocation = IntArray(2)
             val rootGroupLocation = IntArray(2)
@@ -67,13 +94,77 @@ class TestDeleActivity : AppCompatActivity() {
             rootView.getLocationOnScreen(rootGroupLocation)
             val x = pagLocation[0] - rootGroupLocation[0]
             val y = pagLocation[1] - rootGroupLocation[1]
-            mergeCanvas.drawBitmap(pagBitMap,x.toFloat(),y.toFloat(),paint)
+            mergeCanvas.drawBitmap(pagBitMap, x.toFloat(), y.toFloat(), paint)
             pagBitMap.recycle()
             drawingCache.recycle()
             iv.setImageBitmap(mergeBitmap)
+
+
         }
     }
+
+
+    /**
+     * Pixel copy to copy SurfaceView/VideoView into BitMap
+     * Work with Surface View, Video View
+     * Won't work on Normal View
+     */
+//    fun getBitMapFromSurfaceView(videoView: SurfaceView, callback: (Bitmap?) -> Unit) {
+//        val bitmap: Bitmap = Bitmap.createBitmap(
+//            videoView.width,
+//            videoView.height,
+//            Bitmap.Config.ARGB_8888
+//        );
+//        try {
+//            // Create a handler thread to offload the processing of the image.
+//            val handlerThread = HandlerThread("PixelCopier");
+//            handlerThread.start();
+//            PixelCopy.request(
+//                videoView, bitmap,
+//                PixelCopy.OnPixelCopyFinishedListener { copyResult ->
+//                    if (copyResult == PixelCopy.SUCCESS) {
+//                        callback(bitmap)
+//                    }
+//                    handlerThread.quitSafely();
+//                },
+//                Handler(handlerThread.looper)
+//            )
+//        } catch (e: IllegalArgumentException) {
+//            callback(null)
+//            // PixelCopy may throw IllegalArgumentException, make sure to handle it
+//            e.printStackTrace()
+//        }
+//    }
+
+
 }
+
+private fun screenshotSystem(webview:View):Bitmap? {
+    val surfaceClass: Class<*>
+    var method: Method? = null
+    var bitmap: Bitmap? = null
+    try {
+        surfaceClass = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            Class.forName("android.view.SurfaceControl")
+        } else {
+            Class.forName("android.view.Surface")
+        }
+        method = surfaceClass.getDeclaredMethod("screenshot", Integer.TYPE, Integer.TYPE)
+        method.setAccessible(true)
+        bitmap = method.invoke(webview, webview.getWidth(), webview.getHeight()) as Bitmap
+        return bitmap
+    } catch (e: ClassNotFoundException) {
+        e.printStackTrace()
+    } catch (e: NoSuchMethodException) {
+        e.printStackTrace()
+    } catch (e: IllegalAccessException) {
+        e.printStackTrace()
+    } catch (e: InvocationTargetException) {
+        e.printStackTrace()
+    }
+    return null
+}
+
 
 
 interface Subject {
